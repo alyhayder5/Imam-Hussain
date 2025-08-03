110be3ba5e2fb43baea66eb11e3243432457339b
alyhayder5 <alyhayder2005@gmail.com>
2025-08-03 16:15:05 +0500

    fix path issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Footer from '@/components/footer';
-import Header from '@/components/header';
-import Topic from '@/components/topic';
+import Footer from '../components/footer';
+import Header from '../components/header';
+import Topic from '../components/topic';
 
 export default function Home() {
   return (
