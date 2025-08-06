6e0ac73d7641b7a72d18b994691dd2aad4889653
alyhayder5 <alyhayder2005@gmail.com>
2025-08-06 17:04:07 +0500

    added new font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import localFont from 'next/font/local';
 import './globals.css';
 
 const geistSans = Geist({
@@ -10,6 +11,11 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
+});
+
+const beautifulPeople = localFont({
+  src: '../../public/font/BeautifulPeoplePersonalUse-dE0g.ttf',
+  variable: '--font-beautiful-people',
 });
 
 export const metadata: Metadata = {
@@ -26,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${beautifulPeople.variable} antialiased`}
       >
         {children}
       </body>
